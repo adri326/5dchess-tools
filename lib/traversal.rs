18fb9bc396cdb19c2624f4050e443d669e5fec46
Adrien Burgun <adrien.burgun@orange.fr>
2021-01-05 23:20:12 +0100

    :sparkle: Traverse tree in both direction (now well named)

--- lib/traversal.rs
+++ lib/traversal.rs
@@ -1,11 +1,11 @@
 use crate::game::*;
 
-/** Preorder tree traversal algorithm, or "bubble up" algorithm.
-    Traverses the game state by going forward in time.
+/** Preorder tree traversal algorithm, or "bubble down" algorithm (down as in https://www.reddit.com/r/ProgrammerHumor/comments/kk5ng1/finally_after_years_of_search_i_found_a_real_tree/).
+    Traverses a game state by going forward in time.
     Branches as per the arrows, ie. whenever a new timeline emerges from the current board.
 
     This functions borrows boards **mutably** and gives them to a user-defined function.
-    If you wish to borrow the boards immutably, use `bubble_up` instead.
+    If you wish to borrow the boards immutably, use `bubble_down` instead.
 
     ## Example:
 
@@ -17,13 +17,13 @@ use crate::game::*;
     T0w T0b T1w
     ```
 
-    When given this game state as the `game` argument and `(0, 0)` as the `coords` argument, the user-defined `function` will be called on the boards in the following order:
+    When given this game state as the `game` argument and `⟨0, 0⟩` as the `coords` argument, the user-defined `function` will be called on the boards in the following order:
     - `x₁ = function(A, ⟨0, 0⟩, x₀)`
     - `x₂ = function(B, ⟨0, 1⟩, x₁)`
     - `x₃ = function(C, ⟨0, 2⟩, x₂)`
     - `x₄ = function(D, ⟨1, 1⟩, x₁)`
 **/
-pub fn bubble_up_mut<'a, F, T>(
+pub fn bubble_down_mut<'a, F, T>(
     game: &'a mut Game,
     coords: (Layer, Time),
     function: F,
@@ -55,12 +55,12 @@ pub fn bubble_up_mut<'a, F, T>(
     }
 }
 
-/** Preorder tree traversal algorithm, or "bubble up" algorithm.
-    Traverses the game state by going forward in time.
+/** Preorder tree traversal algorithm, or "bubble down" algorithm (down as in https://www.reddit.com/r/ProgrammerHumor/comments/kk5ng1/finally_after_years_of_search_i_found_a_real_tree/).
+    Traverses a game state by going forward in time.
     Branches as per the arrows, ie. whenever a new timeline emerges from the current board.
 
     This functions borrows boards **immutably** and gives them to a user-defined function.
-    If you wish to borrow the boards mutably, use `bubble_up_mut` instead.
+    If you wish to borrow the boards mutably, use `bubble_down_mut` instead.
 
     ## Example:
 
@@ -72,13 +72,13 @@ pub fn bubble_up_mut<'a, F, T>(
     T0w T0b T1w
     ```
 
-    When given this game state as the `game` argument and `(0, 0)` as the `coords` argument, the user-defined `function` will be called on the boards in the following order:
+    When given this game state as the `game` argument and `⟨0, 0⟩` as the `coords` argument, the user-defined `function` will be called on the boards in the following order:
     - `x₁ = function(A, ⟨0, 0⟩, x₀)`
     - `x₂ = function(B, ⟨0, 1⟩, x₁)`
     - `x₃ = function(C, ⟨0, 2⟩, x₂)`
     - `x₄ = function(D, ⟨1, 1⟩, x₁)`
 **/
-pub fn bubble_up<'a, F, T>(game: &'a Game, coords: (Layer, Time), function: F, initial_state: T)
+pub fn bubble_down<'a, F, T>(game: &'a Game, coords: (Layer, Time), function: F, initial_state: T)
 where
     T: Clone,
     F: (Fn(&Board, (Layer, Time), T) -> T) + Copy,
@@ -105,4 +105,104 @@ where
             );
         }
     }
+}
+
+/** Upward tree traversal, or "bubble up" algorithm.
+    Traverses a game state by going backwards in time, unrolling the history of a given board.
+    No branching occurs.
+
+    This functions borrows boards **mutably** and gives them to a user-defined function.
+    If you wish to borrow the boards immutably, use `bubble_up` instead.
+
+    ## Example:
+
+    ```
+     /- D : +1L
+     |
+    A - B - C : 0L
+    ^   ^   ^
+    T0w T0b T1w
+    ```
+
+    When given this game state as the `game` argument and `⟨1, 1⟩` as the `coords` argument, the user-defined `function` will be called on the boards in the following order:
+    - `x₁ = function(D, ⟨1, 1⟩, x₀)`
+    - `x₂ = function(A, ⟨0, 0⟩, x₁)`
+
+    Similarly, if `coords = ⟨0, 2⟩`:
+
+    - `x₁ = function(C, ⟨0, 2⟩, x₀)`
+    - `x₂ = function(B, ⟨0, 1⟩, x₁)`
+    - `x₃ = function(A, ⟨0, 0⟩, x₂)`
+**/
+pub fn bubble_up_mut<'a, F, T>(game: &'a mut Game, mut coords: (Layer, Time), function: F, initial_state: T) -> T
+where
+    T: Clone,
+    F: (Fn(&mut Board, (Layer, Time), T) -> T) + Copy,
+{
+    let mut state = initial_state;
+
+    while let Some(board) = game.boards.get_mut(&coords) {
+        state = function(board, coords, state);
+
+        if let Some(starts_from) = game.info.get_timeline(coords.0).unwrap().starts_from {
+            if starts_from.1 == coords.1 + 1 {
+                coords = starts_from;
+                continue;
+            }
+        }
+
+        coords = (coords.0, coords.1 - 1);
+    }
+
+    state
+}
+
+/** Upward tree traversal, or "bubble up" algorithm.
+    Traverses a game state by going backwards in time, unrolling the history of a given board.
+    No branching occurs.
+
+    This functions borrows boards **immutably** and gives them to a user-defined function.
+    If you wish to borrow the boards mutably, use `bubble_up_mut` instead.
+
+    ## Example:
+
+    ```
+     /- D : +1L
+     |
+    A - B - C : 0L
+    ^   ^   ^
+    T0w T0b T1w
+    ```
+
+    When given this game state as the `game` argument and `⟨1, 1⟩` as the `coords` argument, the user-defined `function` will be called on the boards in the following order:
+    - `x₁ = function(D, ⟨1, 1⟩, x₀)`
+    - `x₂ = function(A, ⟨0, 0⟩, x₁)`
+
+    Similarly, if `coords = ⟨0, 2⟩`:
+
+    - `x₁ = function(C, ⟨0, 2⟩, x₀)`
+    - `x₂ = function(B, ⟨0, 1⟩, x₁)`
+    - `x₃ = function(A, ⟨0, 0⟩, x₂)`
+**/
+pub fn bubble_up<'a, F, T>(game: &'a Game, mut coords: (Layer, Time), function: F, initial_state: T) -> T
+where
+    T: Clone,
+    F: (Fn(&Board, (Layer, Time), T) -> T) + Copy,
+{
+    let mut state = initial_state;
+
+    while let Some(board) = game.boards.get(&coords) {
+        state = function(board, coords, state);
+
+        if let Some(starts_from) = game.info.get_timeline(coords.0).unwrap().starts_from {
+            if starts_from.1 == coords.1 + 1 {
+                coords = starts_from;
+                continue;
+            }
+        }
+
+        coords = (coords.0, coords.1 - 1);
+    }
+
+    state
 }
