c95c4d2006e271084b5a7024c58272ab61ccc0da
Adrien Burgun <adrien.burgun@orange.fr>
2021-01-24 15:44:45 +0100

    :sparkles: ApplyGoal iterator
    [ci skip]

--- lib/utils.rs
+++ lib/utils.rs
@@ -3,6 +3,14 @@ use crate::strategies::legal::OptLegalMove;
 use crate::*;
 use std::time::{Duration, Instant};
 
+// Legality checker
+
+/**
+    Iterator returned by `list_legal_movesets` and `list_legal_movesets_filter_strategy`.
+
+    Returns generated movesets and their corresponding, generated partial game, if they are legal movesets.
+    A strategy can also be provided to filter the moves.
+**/
 pub struct LegalMovesetsIter<'a, S, B>
 where
     S: Strategy<'a, B, From = Move, To = bool>,
@@ -63,8 +71,6 @@ where
         duration
     )
 }
-
-
 
 impl<'a, S, B> LegalMovesetsIter<'a, S, B>
 where
@@ -132,6 +138,130 @@ where
                     }
                 }
                 Some(Err(_)) => {},
+                None => break None,
+            }
+        };
+
+        self.sigma += start.elapsed();
+        res
+    }
+}
+
+
+// Goals
+
+pub struct ApplyGoals<'a, 'b, B, G, I>
+where
+    'a: 'b,
+    B: Clone + AsRef<Board> + 'a,
+    G: Goal<B>,
+    I: Iterator<Item=(Moveset, PartialGame<'a, B>)>,
+{
+    pub iterator: I,
+    pub goal: &'b G,
+    pub game: &'a Game,
+    pub sigma: Duration,
+    pub duration: Option<Duration>,
+    pub depth: usize,
+}
+
+pub fn list_legal_movesets_filter_strategy_goal<'a, 'b, S, G, B>(
+    game: &'a Game,
+    partial_game: &'a PartialGame<'a, B>,
+    goal: &'b G,
+    duration: Option<Duration>,
+    depth: usize,
+) -> ApplyGoals<'a, 'b, B, G, LegalMovesetsIter<'a, S, B>>
+where
+    'a: 'b,
+    S: Strategy<'a, B, From = Move, To = bool>,
+    G: Goal<B>,
+    B: Clone + AsRef<Board> + AsMut<Board> + 'a,
+    for<'c> &'c B: GenMoves<'c, B>,
+    for<'c> B: From<(Board, &'c Game, &'c PartialGame<'c, B>)>,
+{
+    ApplyGoals::new(
+        list_legal_movesets_filter_strategy(game, partial_game, duration),
+        goal,
+        game,
+        duration,
+        depth
+    )
+}
+
+impl<'a, 'b, B, G, I> ApplyGoals<'a, 'b, B, G, I>
+where
+    'a: 'b,
+    B: Clone + AsRef<Board> + 'a,
+    G: Goal<B>,
+    I: Iterator<Item=(Moveset, PartialGame<'a, B>)>,
+{
+    pub fn new(iterator: I, goal: &'b G, game: &'a Game, duration: Option<Duration>, depth: usize) -> Self {
+        Self {
+            iterator,
+            goal,
+            game,
+            sigma: Duration::new(0, 0),
+            duration,
+            depth,
+        }
+    }
+
+    pub fn elapsed(&self) -> Duration {
+        self.sigma
+    }
+
+    pub fn remaining(&self) -> Option<Duration> {
+        match self.duration {
+            Some(duration) => {
+                if self.sigma > duration {
+                    Some(Duration::new(0, 0))
+                } else {
+                    Some(duration - self.sigma)
+                }
+            }
+            None => None
+        }
+    }
+
+    pub fn timed_out(&self) -> bool {
+        match self.duration {
+            Some(duration) => self.sigma > duration,
+            None => false,
+        }
+    }
+}
+
+impl<'a, 'b, B, G, I> Iterator for ApplyGoals<'a, 'b, B, G, I>
+where
+    'a: 'b,
+    B: Clone + AsRef<Board>,
+    G: Goal<B>,
+    I: Iterator<Item=(Moveset, PartialGame<'a, B>)>,
+{
+    type Item = (Moveset, PartialGame<'a, B>);
+
+    fn next(&mut self) -> Option<Self::Item> {
+        let start = Instant::now();
+
+        let res = loop {
+            match self.duration {
+                Some(duration) => {
+                    if self.sigma + start.elapsed() > duration {
+                        break None;
+                    }
+                }
+                None => {}
+            }
+
+            match self.iterator.next() {
+                Some((ms, partial_game)) => {
+                    match self.goal.verify(&ms, self.game, &partial_game, self.depth) {
+                        Some(true) => break Some((ms, partial_game)),
+                        Some(false) => {},
+                        None => break None,
+                    }
+                }
                 None => break None,
             }
         };
