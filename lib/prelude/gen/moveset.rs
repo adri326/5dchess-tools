4acebb3b98c51b604a497a6f5773900a343cd1ae
Adrien Burgun <adrien.burgun@orange.fr>
2021-02-11 22:44:57 +0100

    :fire: Remove partial game stack
    We might revert this commit later on, but this feature was only slowing
    down the algorithm. If it is reintroduced, it'll have to be packaged
    with a robust way of ordering boards.

--- lib/prelude/gen/moveset.rs
+++ lib/prelude/gen/moveset.rs
@@ -230,26 +230,14 @@ pub struct GenLegalMovesetIter<'a> {
     /// A variable-basis state counter, with a special value (None) for "no move"
     states: Vec<Option<usize>>,
 
-    /// A stack of partial games, containing the pre-generated moves for boards[boards.len()-CACHING_DEPTH..]
-    /// It grows in reverse compared to `boards` and `states`; see the comments in `inc` for details about its structure
-    current_partial_games: Vec<PartialGame<'a>>,
-
     // Timing
     sigma: Duration,
     max_duration: Duration,
 
     // Permutation
     physical_moves: Vec<Move>,
-    remaining_physical_moves: Vec<Move>,
-    non_physical_iter: Option<itertools::structs::Permutations<std::vec::IntoIter<(Move, bool)>>>,
+    non_physical_iter: Option<itertools::structs::Permutations<std::vec::IntoIter<Move>>>,
 }
-
-const MIN_CACHING_DEPTH: usize = 4;
-const PG_CACHE_LENGTH: usize = 1;
-
-// This was probably a mistake :/
-// TODO: there are some shady, unnecessary calls to Board::generate_partial_game, where the timeline's last_board was incremented twice.
-// I have changed generate_partial_game so that it doesn't blindly increment it anymore, but these calls should be tracked down.
 
 impl<'a> GenLegalMovesetIter<'a> {
     pub fn new(
@@ -272,12 +260,6 @@ impl<'a> GenLegalMovesetIter<'a> {
             })
             .collect::<Vec<_>>();
         let states = vec![None; boards.len()];
-        let current_partial_games: Vec<PartialGame<'a>> =
-            Vec::with_capacity(if states.len() >= MIN_CACHING_DEPTH {
-                PG_CACHE_LENGTH
-            } else {
-                0
-            });
 
         Self {
             game,
@@ -288,25 +270,12 @@ impl<'a> GenLegalMovesetIter<'a> {
             boards,
             states,
 
-            current_partial_games,
-
             sigma: start.elapsed(),
             max_duration,
 
             physical_moves: vec![],
-            remaining_physical_moves: vec![],
             non_physical_iter: None,
         }
-    }
-
-    fn get_new_partial_game(&self) -> PartialGame<'a> {
-        self.current_partial_games
-            .last()
-            .cloned()
-            .unwrap_or(PartialGame::empty(
-                self.partial_game.info.clone(),
-                Some(self.partial_game),
-            ))
     }
 
     /** Increments the `states`; also updates `current_partial_games`, `physical_moves` and `non_physical_iter` **/
@@ -333,121 +302,21 @@ impl<'a> GenLegalMovesetIter<'a> {
             }
         }
 
-        if self.boards.len() >= MIN_CACHING_DEPTH && index < self.boards.len() && index >= self.boards.len() - PG_CACHE_LENGTH {
-            // Loop to update the PartialGame stack and look for move legality
-            'l: loop {
-                if self.done || start.elapsed() + self.sigma > self.max_duration {
-                    self.sigma += start.elapsed();
-                    return;
-                }
-
-                // Re-construct the PartialGame stack...
-
-                // We wish to have, if index = 4:
-                // P₀, P₁, (P₂), X, X, _, _
-                // Where P₃ is the new, generated partial game, X are partial games to be removed and _ are partial games that aren't on the stack
-                // because of PG_CACHE_LENGTH (here it is set to 5).
-                // If index = 3:
-                // P₀, P₁, P₂, (P₃), X, _, _
-
-                // Note that the partial game stack grows in reverse order; if PG_CACHE_LENGTH = 4, it'd look like that:
-                // Board    = [A, B, C, D, E, F, G]
-                // Partials = [_, _, 4, 3, 2, 1, 0]
-
-                // Add the missing partial games (might happen on some rare carries; I just can't wrap my head around it, though)
-                while self.current_partial_games.len() + index + 1 < self.states.len() {
-                    let new_partial_game = self.get_new_partial_game();
-                    self.current_partial_games.push(new_partial_game);
-                }
-                // Remove the outdated partial games
-                while self.current_partial_games.len() + index >= self.boards.len() {
-                    self.current_partial_games.pop(); // Be gone!
-                }
-
-                // We now add a new partial game. The remaining partial games after that are handled once we exit this loop
-
-                if self.done || start.elapsed() + self.sigma > self.max_duration {
-                    self.sigma += start.elapsed();
-                    return;
-                }
-
-                let mut new_partial_game = self.get_new_partial_game();
-
-                // Try to add the corresponding source board
-                if let Some(i) = self.states[index] {
-                    if let Some(mv) = self.boards[index].get(i) {
-                        mv.generate_partial_game(
-                            self.game,
-                            self.partial_game,
-                            &mut new_partial_game,
-                            PartialGameGenKind::Source,
-                        );
-                    }
-                }
-
-                if let Some(false) = is_in_check(self.game, &new_partial_game) {
-                    self.current_partial_games.push(new_partial_game);
-
-                    // Everything okay, exit the loop
-                    break;
-                } else {
-                    // The position is illegal; no subsequent moves using self.boards[self.states[index]] can be legal, thus we increment
-                    // self.states[index] (and do carries if need be)
-                    // No need to update self.states before index, as they should all be set to None already
-
-                    while index < self.boards.len() {
-                        let n_state = inc_option_usize(self.states[index]);
-                        self.states[index] = Some(n_state);
-
-                        if let None = self.boards[index].get(n_state) {
-                            self.states[index] = None;
-                            index += 1;
-                            if index >= self.boards.len() {
-                                self.done = true;
-                            }
-                        } else {
-                            // Carry done, loop back to update the partial game stack
-                            continue 'l;
-                        }
-                    }
-                }
-            }
-
-            // End of the loop; we add the missing partial games if need be
-            // TODO: maybe don't have this? It'll be done if we need to update the stack anyways
-            while self.current_partial_games.len() <= PG_CACHE_LENGTH {
-                let new_partial_game = self.get_new_partial_game();
-                self.current_partial_games.push(new_partial_game);
-            }
-        }
-
         // Update physical_moves, remaining_physical_moves and non_physical_iter
         let mut physical_moves = Vec::new();
-        let mut remaining_physical_moves = Vec::new();
         let mut non_physical_moves = Vec::new();
         for index in 0..self.states.len() {
             if let Some(state) = self.states[index] {
                 if let Some(mv) = self.boards[index].get(state) {
-                    if self.boards.len() >= MIN_CACHING_DEPTH {
-                        if mv.is_jump() {
-                            non_physical_moves.push((mv, index >= self.boards.len() - PG_CACHE_LENGTH));
-                        } else if index >= self.boards.len() - PG_CACHE_LENGTH {
-                            physical_moves.push(mv);
-                        } else {
-                            remaining_physical_moves.push(mv);
-                        }
+                    if mv.is_jump() {
+                        non_physical_moves.push(mv);
                     } else {
-                        if mv.is_jump() {
-                            non_physical_moves.push((mv, false));
-                        } else {
-                            remaining_physical_moves.push(mv);
-                        }
+                        physical_moves.push(mv);
                     }
                 }
             }
         }
         self.physical_moves = physical_moves;
-        self.remaining_physical_moves = remaining_physical_moves;
 
         let length = non_physical_moves.len();
         self.non_physical_iter = Some(non_physical_moves.into_iter().permutations(length));
@@ -477,41 +346,23 @@ impl<'a> Iterator for GenLegalMovesetIter<'a> {
             }
 
             if let Some(iter) = &mut self.non_physical_iter {
-                if let Some(perm) = iter.next() {
+                if let Some(mut perm) = iter.next() {
                     let mut moves = self.physical_moves.clone();
-                    let mut only_target: Vec<bool> = vec![true; moves.len()];
-
-                    for mv in &self.remaining_physical_moves {
-                        moves.push(mv.clone());
-                        only_target.push(false);
-                    }
-
-                    for (mv, target) in perm {
-                        only_target.push(target);
-                        moves.push(mv);
-                    }
+                    moves.append(&mut perm);
 
                     if let Ok(ms) = Moveset::new(moves, &self.partial_game.info) {
-                        let mut new_partial_game = self.get_new_partial_game();
+                        let mut new_partial_game = PartialGame::empty(self.partial_game.info.clone(), Some(self.partial_game));
 
                         // Generate the remaining boards
                         for i in 0..ms.moves().len() {
                             let mv = &ms.moves()[i];
-                            if only_target[i] {
-                                mv.generate_partial_game(
-                                    self.game,
-                                    self.partial_game,
-                                    &mut new_partial_game,
-                                    PartialGameGenKind::Target,
-                                );
-                            } else {
-                                mv.generate_partial_game(
-                                    self.game,
-                                    self.partial_game,
-                                    &mut new_partial_game,
-                                    PartialGameGenKind::Both,
-                                );
-                            }
+
+                            mv.generate_partial_game(
+                                self.game,
+                                self.partial_game,
+                                &mut new_partial_game,
+                                PartialGameGenKind::Both,
+                            );
                         }
 
                         new_partial_game.info.recalculate_present();
