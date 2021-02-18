56f5fe22e8db8ebce7b60fa83c93b6d09732ca27
Adrien Burgun <adrien.burgun@orange.fr>
2021-02-18 10:12:30 +0100

    :sparkles: Feature for u128 bitboards and have u64 by default

--- lib/prelude/board.rs
+++ lib/prelude/board.rs
@@ -54,7 +54,12 @@ impl Board {
         pieces: Vec<Tile>,
         en_passant: Option<(Physical, Physical)>,
     ) -> Self {
-        let fits_bitboards = (width as u32) * (height as u32) <= 128;
+        // TODO: use `BitBoardPrimitive::BITS` once it becomes stabilized
+        let fits_bitboards = if cfg!(bitboard128) {
+            (width as u32) * (height as u32) <= 128
+        } else {
+            (width as u32) * (height as u32) <= 64
+        };
 
         let bitboards = if fits_bitboards {
             BitBoards::from_pieces(&pieces)
@@ -167,6 +172,13 @@ impl Board {
     }
 }
 
+/// Slower, but supports boards up to 11x11
+#[cfg(bitboard128)]
+pub type BitBoardPrimitive = u128;
+/// Faster, but only supports boards up to 8x8
+#[cfg(not(bitboard128))]
+pub type BitBoardPrimitive = u64;
+
 /** The number of bitboards that there are.
     If your pieces can't be expressed using the basic 5D Chess pieces, you'll have to add new bitboards and increase this amount.
     Currently, there are 11 piece movement kinds that are used:
@@ -191,50 +203,50 @@ pub const N_BITBOARDS: usize = 11;
 #[derive(Clone, Copy, Hash, PartialEq, Eq)]
 pub struct BitBoards {
     // White's pieces
-    pub white: [u128; N_BITBOARDS],
-    pub white_royal: u128,
-    pub white_movable: u128,
+    pub white: [BitBoardPrimitive; N_BITBOARDS],
+    pub white_royal: BitBoardPrimitive,
+    pub white_movable: BitBoardPrimitive,
 
     // Black's pieces
-    pub black: [u128; N_BITBOARDS],
-    pub black_royal: u128,
-    pub black_movable: u128,
+    pub black: [BitBoardPrimitive; N_BITBOARDS],
+    pub black_royal: BitBoardPrimitive,
+    pub black_movable: BitBoardPrimitive,
 }
 
 impl BitBoards {
     pub fn set(&mut self, mask: &BitBoardMask, shift: u32) {
         for n in 0..N_BITBOARDS {
-            self.white[n] = (self.white[n] & !(1u128 << shift)) | (mask.white[n] as u128) << shift;
-            self.black[n] = (self.black[n] & !(1u128 << shift)) | (mask.black[n] as u128) << shift;
+            self.white[n] = (self.white[n] & !(1 << shift)) | (mask.white[n] as BitBoardPrimitive) << shift;
+            self.black[n] = (self.black[n] & !(1 << shift)) | (mask.black[n] as BitBoardPrimitive) << shift;
         }
 
-        self.white_royal = (self.white_royal & !(1u128 << shift)) | (mask.white_royal as u128) << shift;
-        self.black_royal = (self.black_royal & !(1u128 << shift)) | (mask.black_royal as u128) << shift;
+        self.white_royal = (self.white_royal & !(1 << shift)) | (mask.white_royal as BitBoardPrimitive) << shift;
+        self.black_royal = (self.black_royal & !(1 << shift)) | (mask.black_royal as BitBoardPrimitive) << shift;
 
-        self.white_movable = (self.white_movable & !(1u128 << shift)) | (mask.white_movable as u128) << shift;
-        self.black_movable = (self.black_movable & !(1u128 << shift)) | (mask.black_movable as u128) << shift;
+        self.white_movable = (self.white_movable & !(1 << shift)) | (mask.white_movable as BitBoardPrimitive) << shift;
+        self.black_movable = (self.black_movable & !(1 << shift)) | (mask.black_movable as BitBoardPrimitive) << shift;
     }
 
     /// Assumes that `pieces` fits!
     pub fn from_pieces(pieces: &Vec<Tile>) -> Self {
-        let mut white = [0u128; N_BITBOARDS];
-        let mut white_royal = 0u128;
-        let mut white_movable = 0u128;
+        let mut white = [0; N_BITBOARDS];
+        let mut white_royal = 0;
+        let mut white_movable = 0;
 
-        let mut black = [0u128; N_BITBOARDS];
-        let mut black_royal = 0u128;
-        let mut black_movable = 0u128;
+        let mut black = [0; N_BITBOARDS];
+        let mut black_royal = 0;
+        let mut black_movable = 0;
 
         for n in 0..(pieces.len() as u32) {
             let mask = pieces[n as usize].bitboard_mask();
             for o in 0..N_BITBOARDS {
-                white[o] |= (mask.white[o] as u128) << n;
-                black[o] |= (mask.black[o] as u128) << n;
+                white[o] |= (mask.white[o] as BitBoardPrimitive) << n;
+                black[o] |= (mask.black[o] as BitBoardPrimitive) << n;
             }
-            white_royal |= (mask.white_royal as u128) << n;
-            white_movable |= (mask.white_movable as u128) << n;
-            black_royal |= (mask.black_royal as u128) << n;
-            black_movable |= (mask.black_movable as u128) << n;
+            white_royal |= (mask.white_royal as BitBoardPrimitive) << n;
+            white_movable |= (mask.white_movable as BitBoardPrimitive) << n;
+            black_royal |= (mask.black_royal as BitBoardPrimitive) << n;
+            black_movable |= (mask.black_movable as BitBoardPrimitive) << n;
         }
 
         Self {
@@ -255,12 +267,12 @@ impl Default for BitBoards {
             // White's pieces
             white: [0; N_BITBOARDS],
             white_royal: 0,
-            white_movable: !0u128,
+            white_movable: !0,
 
             // Black's pieces
             black: [0; N_BITBOARDS],
             black_royal: 0,
-            black_movable: !0u128,
+            black_movable: !0,
         }
     }
 }
