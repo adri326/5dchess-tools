b173a3a68b1aa9eba4384b88f0aeb8d9acd86da6
Adrien Burgun <adrien.burgun@orange.fr>
2021-03-17 08:48:10 +0100

    :sparkles: Update self_play example

--- examples/self_play/main.rs
+++ examples/self_play/main.rs
@@ -10,8 +10,8 @@ use std::env;
 // const DEPTH: usize = 3;
 const MAX_BRANCHES: usize = 3;
 const MAX_TIMELINES: usize = 8;
-const TIMEOUT: u64 = 30;
-const POOL_SIZE: usize = 1024;
+const TIMEOUT: u64 = 60 * 30;
+const POOL_SIZE: usize = 128;
 const MAX_POOL_SIZE: usize = 100000;
 const N_THREADS: u32 = 14;
 const APPROX: bool = true;
@@ -20,10 +20,10 @@ fn main() {
     let path = env::args().last().unwrap();
     let mut game = read_and_parse_opt(&path).unwrap();
 
-    // let pg = no_partial_game(&game);
-    // for b in pg.own_boards(&game) {
-    //     println!("{:?}", b);
-    // }
+    let pg = no_partial_game(&game);
+    for b in pg.own_boards(&game) {
+        println!("{:?}", b);
+    }
 
     for turn in 0..100 {
         if let Some((node, value)) = iddfs_bl_schedule(
@@ -33,17 +33,33 @@ fn main() {
             .inactive_multiplier(0.05)
             .add(
                 KingSafety::default()
-                .diagonal_empty(0.0)
+                .diagonal_empty(-0.02)
                 .diagonal_opponent(-0.75)
-                .orthogonal_empty(-0.25)
+                .orthogonal_empty(-0.02)
                 .orthogonal_opponent(-1.0)
-                .knight_opponent(-0.5)
+                .knight_opponent(-0.6)
                 .additional_king(-6.0)
+                .inactive_multiplier(0.05)
             ).add(
-                TimelineAdvantage::default()
+                TimelineAdvantage::default(),
             ).add(
                 PawnProgression::default()
-            ),
+                .inactive_multiplier(0.05)
+            ).add(
+                Deepen::default()
+                .none_mult(0.05)
+                .max_time(Duration::new(0, 1_000_000))
+                .eval(
+                    PieceValues::default()
+                    .add(TimelineAdvantage::default())
+                    .add(PawnProgression::default())
+                )
+                .intermediary_value(
+                    PieceValues::default()
+                    .add(TimelineAdvantage::default())
+                )
+            )
+            ,
             TasksOptions::default()
                 .n_threads(N_THREADS)
                 .max_pool_size(MAX_POOL_SIZE)
