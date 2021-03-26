c28ea81d9f2f348726d824cdd6cc3e62b5fb4784
Adrien Burgun <adrien.burgun@orange.fr>
2021-03-26 14:04:12 +0100

    :sparkles: remove and sanity

--- lib/gen/n_cuboids.rs
+++ lib/gen/n_cuboids.rs
@@ -27,7 +27,7 @@ pub fn hc_contains(hypercuboid: HC, point: HashMap<Layer, usize>) -> bool {
     Given a cross section, cut it out of a hypercuboid, returning a list of disjoint hypercuboids that are
     subsets of the original.
 **/
-fn cut(mut hypercuboid: HC, sections: HashMap<Layer, Vec<usize>>) -> Vec<HC> {
+fn cut(mut hypercuboid: HC, sections: &HashMap<Layer, Vec<usize>>) -> Vec<HC> {
     let mut res: Vec<HC> = Vec::with_capacity(sections.len());
 
     for section in sections {
@@ -40,9 +40,9 @@ fn cut(mut hypercuboid: HC, sections: HashMap<Layer, Vec<usize>>) -> Vec<HC> {
 }
 
 /**
-    split a hypercuboid into two 
+    split a hypercuboid into two
 **/
-fn split(hypercuboid: HC, section: (Layer, Vec<usize>)) -> (HC, HC) {
+fn split(hypercuboid: HC, section: (&Layer, &Vec<usize>)) -> (HC, HC) {
     let mut with_hc = hypercuboid.clone();
     let mut without_hc = hypercuboid;
     with_hc
@@ -57,8 +57,18 @@ fn split(hypercuboid: HC, section: (Layer, Vec<usize>)) -> (HC, HC) {
     (with_hc, without_hc)
 }
 
+/**
+    Variant of split, which mutates `hypercuboid` in place
+**/
+fn split_without(hypercuboid: &mut HC, section: (&Layer, &Vec<usize>)) {
+    hypercuboid
+        .get_mut(&section.0)
+        .unwrap()
+        .retain(|index| section.1.iter().find(|target| *target == index).is_none());
+}
 
-#[derive(Clone)]
+
+#[derive(Clone, Debug)]
 pub enum AxisLoc {
     Physical(Board, Move),
     Arrive(Board, Move),
@@ -66,10 +76,27 @@ pub enum AxisLoc {
     Pass(Layer, Option<Time>),
 }
 
+#[derive(Clone, PartialEq, Debug)]
+pub enum Sec {
+    Single(HashMap<Layer, Vec<usize>>),
+    MatchesOne((Layer, Vec<usize>), HashMap<Layer, Vec<usize>>),
+}
+
+impl From<(&Layer, &Vec<usize>)> for Sec {
+    fn from((l, v): (&Layer, &Vec<usize>)) -> Self {
+        let mut hm = HashMap::new();
+        hm.insert(*l, v.clone());
+        Sec::Single(hm)
+    }
+}
+
+#[derive(Clone, Debug)]
 pub struct Search<'a> {
     // Info (parts of it):
     game: &'a Game,
     partial_game: &'a PartialGame<'a>,
+    n_playable: usize,
+    n_branching_axes: Layer,
 
     elements: Vec<AxisLoc>,
     cuboids: Vec<HC>, // Union of cuboids
@@ -86,10 +113,12 @@ impl<'a> Search<'a> {
         let mut branching_axis = Vec::new();
         let mut axes: HashMap<Layer, Vec<usize>> = HashMap::new();
         let mut elements: Vec<AxisLoc> = Vec::new();
+        let mut n_playable = 0;
 
         for board in partial_game.own_boards(game) {
             axes.insert(board.l(), vec![elements.len()]);
-            elements.push(AxisLoc::Pass(board.l(),Some(board.t())) );
+            elements.push(AxisLoc::Pass(board.l(),Some(board.t())));
+            n_playable += 1;
         }
 
         for board in partial_game.own_boards(game) {
@@ -156,10 +185,91 @@ impl<'a> Search<'a> {
         Some(Self {
             game,
             partial_game,
+            n_playable,
+            n_branching_axes,
             elements,
             cuboids: vec![axes],
         })
     }
 
-    // TODO: remove
+    pub fn remove(&mut self, section: Sec) {
+        if let Some(tail_hc) = self.cuboids.pop() {
+            match section {
+                Sec::Single(sec) => {
+                    let res = cut(tail_hc, &sec);
+                    // >>= sanity info
+                    for hc in res.into_iter() {
+                        if let Some(hc) = self.sanity(hc) {
+                            self.cuboids.push(hc);
+                        }
+                    }
+                }
+                Sec::MatchesOne(leave, branches) => {
+                    let (leaving, not_leaving) = split(tail_hc, (&leave.0, &leave.1));
+                    let mut no_branches = leaving;
+                    let mut res = Vec::new(); // All of the hypercuboids with the leaving point and without the branches
+                    let mut exclude = not_leaving; // Hypercuboid with no leave and no branches
+                    for j in branches {
+                        // exactlyOne
+                        let (with_jump, no_jump) = split(no_branches, (&j.0, &j.1));
+                        no_branches = no_jump;
+                        for x in res.iter_mut() {
+                            split_without(x, (&j.0, &j.1));
+                        }
+                        res.push(with_jump);
+                        // foldl' call
+                        exclude = split(exclude, (&j.0, &j.1)).1;
+                    }
+                    res.push(exclude);
+                    // >>= sanity info
+                    for hc in res.into_iter() {
+                        if let Some(hc) = self.sanity(hc) {
+                            self.cuboids.push(hc);
+                        }
+                    }
+                }
+            }
+        } else {
+            panic!("No cuboids!");
+        }
+    }
+
+    pub fn sanity(&self, mut hypercuboid: HC) -> Option<HC> {
+        // if any null hc
+        for l in self.partial_game.info.min_timeline()..=self.partial_game.info.max_timeline() {
+            let axis = hypercuboid.get(&l)?;
+            if axis.len() == 0 {
+                return None
+            }
+        }
+
+        // foldr
+        let mut b = false;
+        for offset in 1..=self.n_branching_axes {
+            let l = if self.partial_game.info.active_player {
+                self.partial_game.info.max_timeline() + offset
+            } else {
+                self.partial_game.info.min_timeline() - offset
+            };
+
+            let axis = hypercuboid.get_mut(&l)?;
+            // (n:ns), where n is head
+            let head = axis.pop()?;
+            if let AxisLoc::Pass(_, _) = self.elements[head] {
+                // (b && isPass(snd n)) -...-> else
+                if !b {
+                    axis.push(head);
+                }
+            } else {
+                b = true;
+                axis.push(head);
+            }
+
+            if axis.len() == 0 {
+                return None
+            }
+        }
+
+        Some(hypercuboid)
+    }
 }
